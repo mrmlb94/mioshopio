5575899f232fd00ee5e8bc4bfb24535c853c3719
icallmereza <m.motallebi73@gmail.com>
2025-12-02 10:50:40 +0100

    initial push

---
+++ src/main/java/com/example/mioshopio/MioshopioApplication.java
@@ -0,0 +1,13 @@
+package com.example.mioshopio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MioshopioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MioshopioApplication.class, args);
+	}
+
+}
